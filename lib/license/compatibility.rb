16e66c983e83ee5042c1323ad116d678fc1bfc8f
Andrew Nesbitt <andrewnez@gmail.com>
2015-11-25 23:12:10 +0000

    Added more GPL variants

--- lib/license/compatibility.rb
+++ lib/license/compatibility.rb
@@ -4,8 +4,8 @@ module License
   module Compatibility
     PUBLIC_DOMAIN = ['PDDL-1.0', 'SAX-PD', 'Unlicense', 'CC0-1.0']
     PERMISSIVE = ['MIT', 'BSD-3-Clause', 'WTFPL', 'BSD-2-Clause', 'ISC', 'Apache-2.0', 'AFL-1.1', 'AFL-1.2', 'AFL-2.0', 'AFL-2.1', 'AFL-3.0', 'Artistic-2.0']
-    WEAK_COPYLEFT = ['LGPL-3.0', 'LGPL-2.0']
-    COPYLEFT = ['GPL-3.0', 'GPL-2.0']
+    WEAK_COPYLEFT = ['LGPL-3.0', 'LGPL-2.0', 'LGPL-2.0+', 'LGPL-2.1', 'LGPL-2.1+', 'LGPL-3.0', 'LGPL-3.0+']
+    COPYLEFT = ['GPL-3.0', 'GPL-2.0', 'GPL-2.0+', 'GPL-3.0+']
     STRONG_COPYLEFT = ['AGPL-1.0', 'AGPL-3.0']
 
     def self.forward_compatiblity(source_license, derivative_license)
